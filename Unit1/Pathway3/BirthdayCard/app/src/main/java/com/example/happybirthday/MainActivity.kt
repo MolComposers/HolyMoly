53dd03f4328ecadfb6c95be1d247e7ea540ecbe5
Cat-JiYoung <kimyamae82@gmail.com>
2023-08-27 21:31:33 +0900

    [FEAT] 텍스트 컴포저블 실습 완료

--- Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,35 +1,18 @@
-/*
- * Copyright (C) 2022 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.happybirthday
 
 import android.os.Bundle
-import android.text.Layout
+import android.view.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -38,23 +21,45 @@ import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent {
+            HappyBirthdayTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    GreetingText(message = "Happy Birthday Sam!", from = "From Emma")
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+        Column(
+            // 가운데 정렬
+            verticalArrangement = Arrangement.Center,
+            modifier = modifier,
+        ) {
+            Text(
+                text = message,
+                fontSize = 36.sp,
+            )
+            Text(
+                text = from,
+                fontSize = 36.sp,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(alignment = Alignment.End),
+            )
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun BirthdayCardPreview() {
+        HappyBirthdayTheme {
+            GreetingText(message = "Happy Birthday Sam!", from = "From Emma")
+        }
     }
 }
-
-// 7. 텍스트 정렬 및 패딩 추가
-@Composable
-fun BirthdayGreetingWithText(message: String, from: String) {
-    // Create a column so that texts don't overlap
-    Column { }
-}
-
-// 5. Box 레이아웃 추
-@Composable
-fun BirthdayGreetingWithImage(message: String, from: String) { }
-
-// 4. 이미지 컴포저블 추가
-@Preview(showBackground = false)
-@Composable
-private fun BirthdayCardPreview() { }
-
